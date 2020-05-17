a8e80a519cc7d3790c1beef4094ceb60eb37c1d4
Rodrigo Cavero Blades <rodrigo.a.cavero.blades.21@dartmouth.edu>
2020-05-17 19:33:31 -0400

    changed server to heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'https://rodrigo-lab5.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://rodrigo-lab5.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 
 // keys for actiontypes
 export const ActionTypes = {
